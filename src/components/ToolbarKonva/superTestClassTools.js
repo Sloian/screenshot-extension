3d9ec9ff06cb0a1cb4bf75812bcd8238629497c7
Sloian <andriysolonynka10@gmail.com>
2023-02-09 14:14:15 +0200

    Rectangle and Text are added to the toolbar

--- src/components/ToolbarKonva/superTestClassTools.js
+++ src/components/ToolbarKonva/superTestClassTools.js
@@ -1,15 +1,20 @@
 
-import React, { useState, useEffect } from "react";
-import { Stage, Layer, Arrow, Circle, Line, Text } from "react-konva";
+import React, { useState } from "react";
+import { Stage, Layer, Arrow, Circle, Line, Text, Rect } from "react-konva";
 
 let startx, starty
 let points = []
 
+
 function TextDrawable(x, y) {
+  return (<Text x={x} y={y} text='Simple Text' fontFamily='Calibri' fill='red' fontSize={30} draggable={true} />);
+}
 
-  return (<Text text='Simple Text' fontFamily='Calibri' fill='green' fontSize={30}
+function RectangleDrawable(x, y) {
+  const width = startx - x;
+  const height = starty - y;
 
-  />
+  return (<Rect x={x} y={y} width={width} height={height} stroke={'red'} strokeWidth={4} draggable />
   );
 }
 
@@ -32,10 +37,14 @@ function FreePathDrawable(x, y) {
   return <Line points={points} strokeWidth={4} fill="red" stroke="red" />;
 }
 
+
+
 const drawableClasses = {
   FreePathDrawable,
   ArrowDrawable,
-  CircleDrawable
+  CircleDrawable,
+  RectangleDrawable,
+  TextDrawable
 };
 
 const getNewDrawableBasedOnType = (x, y, type) => {
@@ -43,50 +52,60 @@ const getNewDrawableBasedOnType = (x, y, type) => {
 };
 
 export const Drawable = () => {
-  const [newDrawableType, setNewDrawableType] = useState(`FreePathDrawable`)
+
   const [drawables, setDrawables] = useState([])
+  const [newDrawableType, setNewDrawableType] = useState(`FreePathDrawable`)
   const [newDrawable, setNewDrawable] = useState([])
+  const [testUndo, setTestUndo] = useState(0)
+
+  const handleMouseDown = e => {
+    if (newDrawable.length === 0) {
+      const { x, y } = e.target.getStage().getPointerPosition();
+      if (!startx) {
+        startx = x
+        starty = y
+      }
+      setNewDrawable([getNewDrawableBasedOnType(
+        x,
+        y,
+        newDrawableType
+      )]);
+    }
+  };
+
+  const handleMouseMove = e => {
+    console.log(newDrawable)
+    if (newDrawable.length === 1) {
+      const { x, y } = e.target.getStage().getPointerPosition();
+      setNewDrawable([drawableClasses[newDrawableType](x, y)])
+    }
+  };
+
+  const handleMouseUp = e => {
+    console.log(newDrawable)
+    if (newDrawable.length === 1) {
+      const drawableToAdd = newDrawable[0];
+      drawables.push(drawableToAdd);
+      console.log(drawables)
+      // setDrawables([...drawables])
+      // drawables.push()
+      console.log(drawables)
+      setNewDrawable([])
+      points = []
+      startx = 0
+      starty = 0
+    }
+  };
+
+
 
   const SuperSceneWithDrawables = () => {
-    const handleMouseDown = e => {
-      if (newDrawable.length === 0) {
-        const { x, y } = e.target.getStage().getPointerPosition();
-        if (!startx) {
-          startx = x
-          starty = y
-        }
-        setNewDrawable([getNewDrawableBasedOnType(
-          x,
-          y,
-          newDrawableType
-        )]);
-      }
-    };
 
-    const handleMouseMove = e => {
-      console.log(newDrawable)
-      if (newDrawable.length === 1) {
-        const { x, y } = e.target.getStage().getPointerPosition();
-        setNewDrawable([drawableClasses[newDrawableType](x, y)])
-      }
-    };
+    const drawableForRender = [...drawables]
+    if (newDrawable.length === 1) {
+      drawableForRender.push(newDrawable)
+    }
 
-    const handleMouseUp = e => {
-      console.log(newDrawable)
-      if (newDrawable.length === 1) {
-        const drawableToAdd = newDrawable[0];
-        drawables.push(drawableToAdd);
-        setDrawables([...drawables], newDrawable)
-        setNewDrawable([])
-        points = []
-        startx = 0
-        starty = 0
-      }
-    };
-
-
-    const drawabless = [...drawables, newDrawable];
-    console.log(drawabless)
     return (
       <div style={{ position: 'absolute' }}>
         <Stage
@@ -98,7 +117,7 @@ export const Drawable = () => {
         >
           <Layer
           >
-            {drawabless.map(drawable => {
+            {drawableForRender.map(drawable => {
               return drawable;
             })}
           </Layer>
@@ -135,9 +154,29 @@ export const Drawable = () => {
           className="button-class"
           style={{ position: 'absolute', top: 0, left: 330, zIndex: 1 }}
           onClick={e => {
-            drawabless.pop()
-            console.log(drawabless)
-            setNewDrawable([...newDrawable])
+            setNewDrawableType("RectangleDrawable")
+          }}
+        >
+          Rectangle
+        </button>
+        <button
+          className="button-class"
+          style={{ position: 'absolute', top: 0, left: 420, zIndex: 1 }}
+          onClick={e => {
+            setNewDrawableType("TextDrawable")
+          }}
+        >
+          Text
+        </button>
+        <button
+          className="button-class"
+          style={{ position: 'absolute', top: 0, left: 480, zIndex: 1 }}
+          onClick={e => {
+            console.log(drawables)
+            console.log(drawableForRender)
+            drawables.pop()
+            // setDrawables(a)
+            setTestUndo(prev => prev + 1)
           }}
         >
           undo
@@ -147,7 +186,6 @@ export const Drawable = () => {
   }
 
   return (<SuperSceneWithDrawables
-    setNewDrawableType={setNewDrawableType}
   />)
 }
 
